23da1574e3506145fe27ac92fea2d368c0af4069
Alexander Smirnov <hfxjr65@gmail.com>
2025-05-17 16:46:58 +0300

    Задание по изменению состояний и рендерингу

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,24 @@
 import { subscribe, unsubscribe } from './resources/API';
+import React, { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const { sourceId } = props;
+    const [lastMessage, setLastMessage] = useState<number>(-1);
+
+    useEffect(() => {
+        const handleMessage = (message: number) => {
+            setLastMessage(message);
+        };
+        subscribe(sourceId, handleMessage);
+        return () => {
+            unsubscribe(sourceId, handleMessage);
+            setLastMessage(-1);
+        };
+    }, [sourceId]);
+
+    return (
+        <div>
+            {sourceId}: {lastMessage}
+        </div>
+    );
 }
